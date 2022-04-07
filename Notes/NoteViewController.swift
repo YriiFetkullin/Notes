ddf162df030d28d0f628b22997f6d3670de1b705
Yrii Fetkullin <02011994jkl@mail.ru>
2022-04-07 23:39:13 +0300

    no message

--- Notes/NoteViewController.swift
+++ Notes/NoteViewController.swift
@@ -22,12 +22,22 @@ class NoteViewController: UIViewController {
         return textNotes
     }()
 
-    private let titleView: UITextField = {
+    private let titleField: UITextField = {
         let notes = UITextField()
         notes.translatesAutoresizingMaskIntoConstraints = false
         return notes
     }()
 
+    private let dateField: UITextField = {
+        let noteDate = UITextField()
+        noteDate.translatesAutoresizingMaskIntoConstraints = false
+        return noteDate
+    }()
+    private var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.draw(CGRect(x: 0, y: 0, width: 30, height: 40))
+        return datePicker
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -40,18 +50,39 @@ class NoteViewController: UIViewController {
         textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
         textView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
         textView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
-        titleView.placeholder = "Введите заголовок"
-        titleView.text = defaults.string(forKey: "title")
-        view.addSubview(titleView)
-        titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        titleView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
-        titleView.bottomAnchor.constraint(equalTo: textView.topAnchor, constant: -16).isActive = true
+
+        titleField.placeholder = "Введите заголовок"
+        titleField.text = defaults.string(forKey: "title")
+        view.addSubview(titleField)
+        titleField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        titleField.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        titleField.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+        view.addSubview(dateField)
+
+        dateField.topAnchor.constraint(equalTo: titleField.bottomAnchor, constant: 16).isActive = true
+        dateField.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        dateField.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+        dateField.bottomAnchor.constraint(equalTo: textView.topAnchor, constant: -16).isActive = true
+        dateField.placeholder = "Текущая дата"
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(self.dateChanged), for: .allEvents)
+        dateField.inputView = datePicker
+        datePicker.datePickerMode = .date
+        let localeID = Locale.preferredLanguages.first
+        datePicker.locale = Locale(identifier: localeID!)
 
         navigationItem.rightBarButtonItem = barButton
         barButton.target = nil
         barButton.action = #selector(barButtonTapped(_:))
     }
-
+    func getDateFromPicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMMM yyyy"
+        dateField.text = formatter.string(from: datePicker.date)
+    }
+    @objc func dateChanged() {
+        getDateFromPicker()
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         registerForKeyboardNotifications()
@@ -85,8 +116,8 @@ class NoteViewController: UIViewController {
     @objc func keyboardWillShow(_ notification: Notification) {
         let userInfo = notification.userInfo
         guard let infoKey = userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-        let kbFrameSize = infoKey.cgRectValue
-        textView.contentInset.bottom = kbFrameSize.height
+        let keyboardFrameSize = infoKey.cgRectValue
+        textView.contentInset.bottom = keyboardFrameSize.height
     }
 
     @objc func keyboardWillHide() {
@@ -96,13 +127,21 @@ class NoteViewController: UIViewController {
     @objc func barButtonTapped(_ sender: UIBarButtonItem) {
         view.endEditing(true)
         let notes = textView.text!
-        let title = titleView.text!
+        let title = titleField.text!
+        let date = dateField.text!
 
-        if !notes.isEmpty && !title.isEmpty {
+        if !notes.isEmpty && !title.isEmpty && !date.isEmpty {
             defaults.set(notes, forKey: "notes")
             defaults.set(title, forKey: "title")
-
+            defaults.set(date, forKey: "date")
             textView.resignFirstResponder()
+            titleField.resignFirstResponder()
+            dateField.resignFirstResponder()
         }
+    }
+    func configureElements(model: NotesModel) {
+        titleField.text = model.title
+        textView.text = model.notes
+        dateField.text = model.date
     }
 }
---
+++ Notes/NotesModel.swift
@@ -0,0 +1,15 @@
+//
+//  NotesModel.swift
+//  Notes
+//
+//  Created by NarkoDiller on 06.04.2022.
+//
+
+import Foundation
+import UIKit
+
+struct NotesModel {
+    var title: String?
+    var notes: String?
+    var date: String?
+}
